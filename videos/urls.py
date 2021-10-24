554e8b0f2a4d371e87d03019d5abc0bfae148ba1
Kamaljeet <kamaljeetsahoo@gmail.com>
2021-10-24 23:03:00 +0530

    UI upadated for sidebar and video upload

--- videos/urls.py
+++ videos/urls.py
@@ -1,5 +1,7 @@
-from django.urls import path, include
+from django.urls import path
+from .views import browse_videos, video_upload
 
 urlpatterns = [
-    
+    path('videos/', browse_videos, name="browse_videos"),
+    path('upload_video/', video_upload, name="video_upload")
 ]
--- videos/views.py
+++ videos/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def video_upload(request):
+    return render(request, 'videos/upload_video.html')
+
+def browse_videos(request):
+    return render(request, 'videos/browse_videos.html')
+
+def view_single_video_content(request, video_id):
+    pass
