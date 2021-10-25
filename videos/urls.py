c5789a3b17cc122d359dc79da4cadae884fcba1f
Kamaljeet <kamaljeetsahoo@gmail.com>
2021-10-25 20:44:37 +0530

    function and url updates

--- videos/urls.py
+++ videos/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import browse_videos, video_upload
+from .views import browse_videos, video_upload, handle_upload_video
 
 urlpatterns = [
     path('videos/', browse_videos, name="browse_videos"),
-    path('upload_video/', video_upload, name="video_upload")
+    path('upload_video/', video_upload, name="video_upload"),
+    path('handle_upload/', handle_upload_video), 
 ]
--- videos/views.py
+++ videos/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 # Create your views here.
 def video_upload(request):
     return render(request, 'videos/upload_video.html')
+
+def handle_upload_video(request):
+    return redirect('browse_videos')
 
 def browse_videos(request):
     return render(request, 'videos/browse_videos.html')
