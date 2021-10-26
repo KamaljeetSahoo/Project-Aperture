5d6d5e7ca625fb2ac4603786432a005f95648350
Kamaljeet <50160354+KamaljeetSahoo@users.noreply.github.com>
2021-10-26 13:09:17 +0530

    Edit page for each extracted frame added

--- videos/urls.py
+++ videos/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import browse_videos, video_upload, handle_upload_video, view_single_video_content
+from .views import browse_videos, video_upload, handle_upload_video, view_single_video_content, edit_video_frame_view
 
 urlpatterns = [
     path('videos/', browse_videos, name="browse_videos"),
     path('upload_video/', video_upload, name="video_upload"),
     path('handle_upload/', handle_upload_video),
-    path('video_view/<int:video_id>/', view_single_video_content)
+    path('video_view/<int:video_id>/', view_single_video_content),
+    path('edit_video_frame/<int:image_id>/', edit_video_frame_view, name="edit_video_frame")
 ]
--- videos/views.py
+++ videos/views.py
@@ -10,15 +10,18 @@ from .models import Video, ExtractedFrame, FrameTag, FrameCaption
 from .utils import extract_frames_from_video
 
 # Create your views here.
+
+
 def video_upload(request):
     if request.user.is_authenticated:
         return render(request, 'videos/upload_video.html')
     else:
         return redirect('login')
 
+
 def handle_upload_video(request):
     if request.user.is_authenticated:
-        vid = Video(video = request.FILES['uploaded_video_file'])
+        vid = Video(video=request.FILES['uploaded_video_file'])
         vid.save()
         extract_frames_from_video(vid.video.path, num_frames=4)
 
@@ -27,30 +30,30 @@ def handle_upload_video(request):
             img_io = BytesIO()
             img.save(img_io, format="JPEG", quality=100)
             img_content = ContentFile(img_io.getvalue(), file)
-            img = ExtractedFrame(image = img_content)
+            img = ExtractedFrame(image=img_content)
             img.save()
             vid.extracted_image.add(img)
 
             _, captions = generate_caption(img.image.url)
             for cap in captions:
-                new_cap = FrameCaption(description = cap)
+                new_cap = FrameCaption(description=cap)
                 new_cap.save()
                 img.caption.add(new_cap)
-            
+
             tags = generate_tags(img.image.url)
             for tag in tags:
                 if not FrameTag.objects.filter(tag_name=tag).exists():
-                    new_tag = FrameTag(tag_name = tag)
+                    new_tag = FrameTag(tag_name=tag)
                     new_tag.save()
                     img.tag.add(new_tag)
                 else:
-                    t = FrameTag.objects.get(tag_name = tag)
+                    t = FrameTag.objects.get(tag_name=tag)
                     img.tag.add(t)
-
 
         return redirect('browse_videos')
     else:
         return redirect('login')
+
 
 def browse_videos(request):
     if request.user.is_authenticated:
@@ -62,6 +65,7 @@ def browse_videos(request):
     else:
         return redirect('login')
 
+
 def view_single_video_content(request, video_id):
     if request.user.is_authenticated:
         video = Video.objects.get(id=video_id)
@@ -69,5 +73,15 @@ def view_single_video_content(request, video_id):
             'video': video,
         }
         return render(request, 'videos/single_view_video.html', context=context)
+    else:
+        return redirect('login')
+
+
+def edit_video_frame_view(request, image_id):
+    if request.user.is_authenticated:
+        context = {
+            'img': ExtractedFrame.objects.get(id=image_id)
+        }
+        return render(request, 'videos/edit_contents.html', context=context)
     else:
         return redirect('login')
